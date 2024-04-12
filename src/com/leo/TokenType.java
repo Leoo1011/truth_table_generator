8fc4be5345a7555477ed272c3daf844594fb33de
Leandro Araujo <43557954+Leoo1011@users.noreply.github.com>
2024-04-12 01:32:26 -0300

    Added prettyName's to the enum, to be used in ParseError's.

--- src/com/leo/TokenType.java
+++ src/com/leo/TokenType.java
@@ -2,15 +2,24 @@ package com.leo;
 
 public enum TokenType {
     PROP_NAME,
-    OR,
-    AND,
-    XOR,
-    THEN, // "implies" == ->
-    IFF,  // <->
-    NOT,
+    OR("|"),
+    AND("&"),
+    XOR("^"),
+    THEN("->"), // "implies" == ->
+    IFF("<->"),  // <->
+    NOT("!"),
     LEFT_PAREN,
     RIGHT_PAREN,
     EOL;   // "end of line"
+
+    TokenType() {
+        this.prettyName = null;
+    }
+
+    public final String prettyName;
+    TokenType(String prettyName) {
+        this.prettyName = prettyName;
+    }
 
     public String toString() {
         return this.name().toLowerCase();
