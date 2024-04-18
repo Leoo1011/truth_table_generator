94963783d434e2d9fbeb642f22560afdb43164a0
Leandro Araujo <43557954+Leoo1011@users.noreply.github.com>
2024-04-18 02:20:38 -0300

    Rewrote method to have it return int[][], and to be more concise.
    Switched to using bitwise operations to extract the bits.

--- src/com/leo/TruthTable.java
+++ src/com/leo/TruthTable.java
@@ -1,41 +1,38 @@
 package com.leo;
 
-import java.util.Objects;
-
 public class TruthTable {
-    public TruthTable() {
-    }
-
-    /**
-     * Pads string s from the left, with fillChar.
-     * @param text String to pad
-     * @param length total desired length of the returned String
-     * @return the String s, padded from the left, with 0s
-     */
-    private static String zeroPadLeft(String text, int length) {
-        String str = Objects.requireNonNull(text);
-        String padding = "0";
-        if (str.length() < length) {
-            int lengthDiff = length - str.length();
-            return padding.repeat(lengthDiff) + str;
-        }
-        return str;
+    private TruthTable() {
     }
 
     /**
      * Generates every permutation of values for the propositions in a truth table
      * with nProps propositions.
      * @param nProps the amount of propositions.
-     * @return String array containing each permutation in the form of 1's (true) and 0's (false).
+     * @return array of arrays of ints containing each permutation in the form of 1's (true) and 0's (false).
      * An example is:
-     * <code>generatePropositionPermutations(2) == {"00", "01", "10", "11"}</code>
+     * <code>generatePropositionPermutations(2) == {{0, 0}, {0, 1}, {1, 0}, {1, 1}}</code>
+     *
+     * @implNote Counting from 0 to 3 (in binary) is:
+     * <ul>
+     * <li>00 (F, F)</li>
+     * <li>01 (F, T)</li>
+     * <li>10 (T, F)</li>
+     * <li>11 (T, T)</li>
+     * </ul>
+     * This algorithm uses this fact, along with bitwise operations (and masking) to extract each individual
+     * bit (truth value).
      */
-    public static String[] generatePropositionPermutations(int nProps) {
-        int numPerms = ((int) Math.pow(2, nProps));
-        String[] permutations = new String[numPerms];
-        for (int i = 0; i < numPerms; i++) {
-            String bin = Integer.toBinaryString(i);
-            permutations[i] = zeroPadLeft(bin, nProps);
+    public static int[][] generateTruthValuePermutations(int nProps) {
+        int nPerms = ((int) Math.pow(2, nProps));
+        int[][] permutations = new int[nPerms][nProps];
+        for (int num = 0; num < nPerms; num++) {
+            for (int i = 0; i < nProps; i++) {
+                int mask = 1 << (nProps - i - 1); // To extract bits from right to left.
+                // the '&' extracts the bit, dividing by 'mask' trims leading 0s (shifting).
+                int extractedBit = (num & mask) / mask;
+                if (extractedBit == 0) continue;
+                permutations[num][i] = extractedBit;
+            }
         }
         return permutations;
     }
