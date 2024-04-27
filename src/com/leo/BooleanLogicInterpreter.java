3bca69216bb365f7b03c8d610442fff1bf8b1c4e
Leandro Araujo <43557954+Leoo1011@users.noreply.github.com>
2024-04-27 01:54:09 -0300

    Wrote the interpreter.

--- src/com/leo/BooleanLogicInterpreter.java
+++ src/com/leo/BooleanLogicInterpreter.java
@@ -1,8 +1,74 @@
 package com.leo;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class BooleanLogicInterpreter {
-    final Expr ast;
-    BooleanLogicInterpreter(Expr parsedFormula) {
-        ast = parsedFormula;
+    private BooleanLogicInterpreter() {
+    }
+
+    private static Map<String, Integer> propValues;
+    private static Expr ast;
+    public static int interpret(final Expr ast, int[] truthValues) {
+        BooleanLogicInterpreter.ast = Objects.requireNonNull(ast);
+
+        propValues = new LinkedHashMap<>();
+        String[] propNames = BooleanLogicParser.getPropositionNames().toArray(new String[0]);
+        for (int i = 0; i < propNames.length; i++) {
+            propValues.put(propNames[i], truthValues[i]);
+        }
+        return interpret(ast);
+    }
+
+    private static int interpret(final Expr ast) {
+        return switch (ast) {
+            case Expr.BinaryOperation binaryOp -> interpret(binaryOp);
+            case Expr.UnaryOperation unaryOp -> interpret(unaryOp);
+            case Expr.Grouping grouping -> interpret(grouping);
+            case Expr.Proposition proposition -> interpret(proposition);
+            default -> throw new IllegalStateException("Unexpected value: " + ast);
+        };
+    }
+
+    private static int xor(int a, int b) {
+        return (a + b) % 2;
+    }
+
+    private static int not(int a) {
+        int no = 0;
+        return Math.abs(~a); // ~0 = -1
+    }
+
+    private static int iff(int a, int b) {
+        return not(xor(a, b));
+    }
+
+    private static int interpret(final Expr.BinaryOperation binaryOp) {
+        int left = interpret(binaryOp.left);
+        int right = interpret(binaryOp.right);
+
+        return switch (binaryOp.operator.type) {
+            case OR   -> left | right;
+            case AND  -> left & right;
+            case XOR  -> xor(left, right);
+            case THEN -> not(left) | right;
+            case IFF  -> iff(left, right);
+            case NOT, PROP_NAME, LEFT_PAREN, RIGHT_PAREN, EOL ->
+                throw new IllegalStateException("Unexpected operator type: '" + binaryOp.operator.type + "'");
+        };
+    }
+
+    private static int interpret(final Expr.UnaryOperation unaryOp) {
+        int propValue = interpret(unaryOp.expression);
+        return not(propValue);
+    }
+
+    private static int interpret(final Expr.Grouping grouping) {
+        return interpret(grouping.expression);
+    }
+
+    private static int interpret(final Expr.Proposition prop) {
+        return propValues.get(prop.propositionName.name);
     }
 }
