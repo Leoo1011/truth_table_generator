2d9b788ea99ddc16c687a490f3685cd8f3f29596
Leandro Araujo <43557954+Leoo1011@users.noreply.github.com>
2024-05-11 00:16:25 -0300

    Added error handling, and documented the "interpret" method.

--- src/com/leo/main/BooleanLogicInterpreter.java
+++ src/com/leo/main/BooleanLogicInterpreter.java
@@ -10,8 +10,25 @@ public class BooleanLogicInterpreter {
 
     private static Map<String, Integer> propValues;
     private static Expr ast;
-    public static int interpret(final Expr ast, int[] truthValues) {
+
+    /**
+     * Interprets the provided Abstract Syntax Tree according to the truth values provided.
+     *
+     * @param ast the abstract syntax tree, as produced by the {@link BooleanLogicParser}.
+     * @param truthValues int[] of 0s and 1s, representing each proposition's truth value.
+     *                    The values are taken in order of appearance: in the formula "A->B",
+     *                    the first value will go to A, the second, to B.
+     *
+     * @return the result of interpreting the ast with the provided truth values, as an int.
+     *
+     * @throws IllegalArgumentException if the number of truth values and propositions in the ast don't match.
+     */
+    public static int interpret(final Expr ast, int[] truthValues) throws IllegalArgumentException {
         BooleanLogicInterpreter.ast = Objects.requireNonNull(ast);
+        if (truthValues.length != BooleanLogicParser.getPropositionNames().size()) {
+            throw new IllegalArgumentException(
+                    "The number of truth values provided should be equal to the number of propositions in the ast.");
+        }
 
         propValues = new LinkedHashMap<>();
         String[] propNames = BooleanLogicParser.getPropositionNames().toArray(new String[0]);
